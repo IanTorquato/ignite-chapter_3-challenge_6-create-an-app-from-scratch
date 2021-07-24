c6ef6c67fec9ae4d6061a9a2e954eb9732303f8d
Ian Torquato <iantorquato2@gmail.com>
2021-07-24 20:26:30 -0300

    Adding global styles, images and Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,18 @@
-export default function Header() {
-  // TODO
+import { ReactElement } from 'react';
+import Link from 'next/link';
+
+import styles from './header.module.scss';
+
+export default function Header(): ReactElement {
+  return (
+    <header className={styles.container}>
+      <nav className={styles.content}>
+        <Link href="/">
+          <a>
+            <img src="/logo.svg" alt="logo" />
+          </a>
+        </Link>
+      </nav>
+    </header>
+  );
 }
