9a37bfd3cbf85b58af99de60a77829ed27f92414
Ian Torquato <iantorquato2@gmail.com>
2021-07-24 23:55:11 -0300

    Adding static content and basic page styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,16 @@
 import { GetStaticProps } from 'next';
+import Link from 'next/link';
+import { ReactElement } from 'react';
+import { FiCalendar, FiUser } from 'react-icons/fi';
 
 import { getPrismicClient } from '../services/prismic';
 
-import commonStyles from '../styles/common.module.scss';
+// import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
+
+/*
+  Utilizar o método query para retornar todos os posts já com paginação. Por padrão, a paginação vem configurada como 20. Portanto se quiser testar sem ter que criar mais de 20 posts, altere a opção pageSize para o valor que deseja.
+*/
 
 interface Post {
   uid?: string;
@@ -24,9 +31,57 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home(): ReactElement {
+  return (
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <img src="/logo.svg" alt="logo" />
+      </header>
+
+      <div>
+        <Link href="/post/1">
+          <a className={styles.post}>
+            <h1>Como utilizar Hooks</h1>
+
+            <p>Pensando em sincronização em vez de ciclos de vida.</p>
+
+            <div>
+              <div>
+                <FiCalendar /> 15 Mar de 2021
+              </div>
+
+              <div>
+                <FiUser /> Joseph Oliveira
+              </div>
+            </div>
+          </a>
+        </Link>
+
+        <Link href="/post/1">
+          <a className={styles.post}>
+            <h1>Como utilizar Hooks</h1>
+
+            <p>Pensando em sincronização em vez de ciclos de vida.</p>
+
+            <div>
+              <div>
+                <FiCalendar /> 15 Mar de 2021
+              </div>
+
+              <div>
+                <FiUser /> Joseph Oliveira
+              </div>
+            </div>
+          </a>
+        </Link>
+      </div>
+
+      <button className={styles.morePosts} type="button">
+        Carregar mais posts
+      </button>
+    </div>
+  );
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,9 +1,15 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
+import { ReactElement } from 'react';
+import { FiCalendar, FiClock, FiUser } from 'react-icons/fi';
 
+import Header from '../../components/Header';
 import { getPrismicClient } from '../../services/prismic';
-
 import commonStyles from '../../styles/common.module.scss';
 import styles from './post.module.scss';
+
+/*
+  Utilizar o método query para buscar todos os posts e o getByUID para buscar as informações do post específico.
+*/
 
 interface Post {
   first_publication_date: string | null;
@@ -26,9 +32,65 @@ interface PostProps {
   post: Post;
 }
 
-// export default function Post() {
-//   // TODO
-// }
+export default function Post(): ReactElement {
+  return (
+    <>
+      <Header />
+
+      <img
+        className={styles.banner}
+        src="https://images8.alphacoders.com/115/1156488.png"
+        alt=""
+      />
+
+      <article className={styles.post}>
+        <h1>Criando um app CRA do zero</h1>
+
+        <div className={styles.postInfos}>
+          <div>
+            <FiCalendar /> 15 Mar 2021
+          </div>
+
+          <div>
+            <FiUser /> Joseph Oliveira
+          </div>
+
+          <div>
+            <FiClock /> 4 min
+          </div>
+        </div>
+
+        <main className={styles.postContent}>
+          <div>
+            <h2>Proin et varius</h2>
+
+            <p className="content">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+              dolor sapien, vulputate eu diam at, condimentum hendrerit tellus.
+              Nam facilisis sodales felis, pharetra pharetra lectus auctor sed.
+              Ut venenatis mauris vel libero pretium, et pretium ligula
+              faucibus. Morbi nibh felis, elementum a posuere et, vulputate et
+              erat. Nam venenatis.
+            </p>
+          </div>
+
+          <div>
+            <h2>Cras laoreet mi</h2>
+
+            <p className="content">
+              Nulla auctor sit amet quam vitae commodo. Sed risus justo,
+              vulputate quis neque eget, dictum sodales sem. In eget felis
+              finibus, mattis magna a, efficitur ex. Curabitur vitae justo
+              consequat sapien gravida auctor a non risus. Sed malesuada mauris
+              nec orci congue, interdum efficitur urna dignissim. Vivamus cursus
+              elit sem, vel facilisis nulla pretium consectetur. Nunc congue.
+            </p>
+          </div>
+        </main>
+      </article>
+    </>
+  );
+}
 
 // export const getStaticPaths = async () => {
 //   const prismic = getPrismicClient();
